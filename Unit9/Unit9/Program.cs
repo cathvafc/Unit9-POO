c5c540fbba98f1a60af0c64a11573e32d516be14
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-05 09:33:26 +0100

    Exercise 1. Unit 9.

---
+++ Unit9/Unit9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Unit9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Alumno alumno = new Alumno("Pepito", 18);
+            alumno.MayorDeEdad();
+            Console.WriteLine("Su edad es: {0}", alumno.getEdad());
+            Console.WriteLine("Su nombre es: {0}", alumno.getNombre());
+
+
+            Alumno alumno1 = new Alumno("Jorgito", 15);
+            alumno1.MayorDeEdad();
+            Console.WriteLine("Su edad es: {0}", alumno1.getEdad());
+            Console.WriteLine("Su nombre es: {0}", alumno1.getNombre());
+        }
+    }
+}
---
+++ Unit9/Unit9/dto/Alumno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit9
+{
+    class Alumno
+    {
+
+        private string nombre = "";
+      
+        private int edad = 0; 
+
+        public Alumno(string nombre, int edad)
+        {
+            this.edad = edad;
+            this.nombre = nombre;
+
+        }
+
+        public  void MayorDeEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine(" Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine(" No es mayor de edad");
+            }
+        }
+
+        public int getEdad()
+        {
+            return edad;
+        }
+        public void setEdad(int edad)
+        {
+            this.edad = edad; 
+        }
+        public string getNombre()
+        {
+            return nombre;
+        }
+        public void setNombre(int edad)
+        {
+            this.edad = edad;
+        }
+
+    }
+}
