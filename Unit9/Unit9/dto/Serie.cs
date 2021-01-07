f9de52d5114e5ef77fbb5d7cd0e4bf1d8cd3c158
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-07 10:42:58 +0100

    Exercise 10

---
+++ Unit9/Unit9/dto/Serie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit9.dto
+{
+    class Serie
+    {
+        //Atributos
+        private string titulo = "";
+        private int numTemporadas = 3;
+        private bool entregado = false;
+        private string genero = "";
+        private string creador = "";
+
+        public Serie()
+        {
+
+        }
+
+        public Serie(string titulo, string creador)
+        {
+            this.titulo = titulo;
+            this.creador = creador;
+        }
+
+        public Serie(string titulo, string creador, int numTemporadas, string genero)
+        {
+            this.titulo = titulo;
+            this.creador = creador;
+            this.numTemporadas = numTemporadas;
+            this.genero = genero;
+        }
+
+    }
+}
