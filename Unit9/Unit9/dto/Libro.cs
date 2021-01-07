b635e7f1a553aee79d794ff9827578e5fbf5e6cf
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-07 10:04:23 +0100

    Exercise 7

--- Unit9/Unit9/dto/Libro.cs
+++ Unit9/Unit9/dto/Libro.cs
@@ -11,18 +11,18 @@ namespace Unit9.dto
         private string titulo = "";
         private string ubicacion = "";
 
-        static void Main(string[] args)       
-        {
-            Libro libro1 = new Libro();
+        //static void Main(string[] args)       
+        //{
+        //    Libro libro1 = new Libro();
 
-            libro1.setAutor("Yo");
-            libro1.setTitulo("Tu");
-            libro1.setUibicacion("Aquí");
+        //    libro1.setAutor("Yo");
+        //    libro1.setTitulo("Tu");
+        //    libro1.setUibicacion("Aquí");
 
-            Console.WriteLine("El autor del libro es: {0}", libro1.getAutor());
-            Console.WriteLine("El titulo del libro es: {0}", libro1.getTitulo());
-            Console.WriteLine("La ubicación del libro es: {0}", libro1.getUbicacion());
-        }
+        //    Console.WriteLine("El autor del libro es: {0}", libro1.getAutor());
+        //    Console.WriteLine("El titulo del libro es: {0}", libro1.getTitulo());
+        //    Console.WriteLine("La ubicación del libro es: {0}", libro1.getUbicacion());
+        //}
 
         public string getAutor()
         {
--- Unit9/Unit9/dto/Persona.cs
+++ Unit9/Unit9/dto/Persona.cs
@@ -6,13 +6,36 @@ namespace Unit9.dto
 {
     class Persona
     {
-        string name = "";
+        const string H = "Hombre";
+        const string M = "Mujer";
+
+        private string name = "";
+        private int edad = 0;
+        private string dni = "";
+        private string sexo = H;
+        private double peso = 0;
+        private double altura = 0;
         public Persona()
         {
             
         }
-       
-        
+        public Persona(string name, int edad, string sexo)
+        {
+            this.name = name;
+            this.edad = edad;
+            this.sexo = sexo;
+
+        }
+
+        public Persona(string name, int edad, string sexo, double peso, double altura, string dni)
+        {
+            this.name = name;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+            this.dni = dni;
+        }
         public void setNombre(string name)
         {
             this.name = name;
