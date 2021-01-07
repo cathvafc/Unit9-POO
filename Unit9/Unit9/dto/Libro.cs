1f8d4d5501e07d82106b1cb91619a0a8c63f0030
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-07 09:28:57 +0100

    Exercise 5

---
+++ Unit9/Unit9/dto/Libro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit9.dto
+{
+
+    class Libro
+    {
+        private string autor = "";
+        private string titulo = "";
+        private string ubicacion = "";
+
+        static void Main(string[] args)       
+        {
+            Libro libro1 = new Libro();
+
+            libro1.setAutor("Yo");
+            libro1.setTitulo("Tu");
+            libro1.setUibicacion("Aquí");
+
+            Console.WriteLine("El autor del libro es: {0}", libro1.getAutor());
+            Console.WriteLine("El titulo del libro es: {0}", libro1.getTitulo());
+            Console.WriteLine("La ubicación del libro es: {0}", libro1.getUbicacion());
+        }
+
+        public string getAutor()
+        {
+            return autor;
+        }
+        public string getTitulo()
+        {
+            return titulo;
+        }
+        public string getUbicacion()
+        {
+            return ubicacion;
+        }
+
+        public void setAutor(string autor)
+        {
+            this.autor = autor;
+        }
+
+        public void setTitulo(string titulo)
+        {
+            this.titulo = titulo;
+        }
+
+        public void setUibicacion(string ubicacion)
+        {
+            this.ubicacion = ubicacion;
+        }
+    }
+}
--- Unit9/Unit9/dto/PruebaPersona.cs
+++ Unit9/Unit9/dto/PruebaPersona.cs
@@ -6,18 +6,18 @@ namespace Unit9.dto
 {
     class PruebaPersona
     {
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
-            Persona persona1 = new Persona();
-            Persona persona2 = new Persona();
+        //    Persona persona1 = new Persona();
+        //    Persona persona2 = new Persona();
 
-            persona1.setNombre("Pepito");
-            persona2.setNombre("Jorgito");
+        //    persona1.setNombre("Pepito");
+        //    persona2.setNombre("Jorgito");
 
-            persona1.Saludar();
-            persona2.Saludar();
+        //    persona1.Saludar();
+        //    persona2.Saludar();
 
-        }
+        //}
     }
 }
