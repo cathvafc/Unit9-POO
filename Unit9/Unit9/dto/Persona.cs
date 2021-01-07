502309c06b7abb1d1f70af76ecdb47719bc05b8f
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-07 09:19:22 +0100

    Exercise 4

---
+++ Unit9/Unit9/dto/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit9.dto
+{
+    class Persona
+    {
+        string name = "";
+        public Persona()
+        {
+            
+        }
+       
+        
+        public void setNombre(string name)
+        {
+            this.name = name;
+        }
+        public void Saludar()
+        {
+            Console.WriteLine("Hola, soy {0}", name);
+        }
+    }
+}
---
+++ Unit9/Unit9/dto/PruebaPersona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit9.dto
+{
+    class PruebaPersona
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona1 = new Persona();
+            Persona persona2 = new Persona();
+
+            persona1.setNombre("Pepito");
+            persona2.setNombre("Jorgito");
+
+            persona1.Saludar();
+            persona2.Saludar();
+
+        }
+    }
+}
